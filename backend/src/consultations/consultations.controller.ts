152023b66b93700b53af754b2d9361a339a3c428
brnoliveira110 <brnoliveirasantana@gmail.com>
2025-12-22 00:49:19 -0300

    chore: Remove `db` and `backend` services and `postgres_data` volume from `docker-compose.yml`.

--- backend/src/consultations/consultations.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('consultations')
-export class ConsultationsController {}
--- backend/src/vaccines/vaccines.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('vaccines')
-export class VaccinesController {}
